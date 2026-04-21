7bc1135527780492961c11b2dbb908ded9d0331c
Charan-Amuru <52287645+CHARANSAIAMURU@users.noreply.github.com>
2026-04-21 04:20:21 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,7 +15,6 @@ const app = express();
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 5000;
-const CLIENT_ORIGIN = "*";
 
 const io = new Server(server, {
   cors: {
@@ -25,7 +24,6 @@ const io = new Server(server, {
 });
 
 app.use(cors());
-
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -44,9 +42,7 @@ app.use("/api/trucks", truckRoutes);
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
-  .catch((err) => {
-    console.error("MongoDB connection error:", err.message);
-  });
+  .catch((err) => console.error("MongoDB connection error:", err.message));
 
 setInterval(async () => {
   try {
