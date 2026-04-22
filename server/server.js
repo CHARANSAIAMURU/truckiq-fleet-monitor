7b3b59f0e54c39e8f11e1cd2c0f332682fbd5dd3
Charan-Amuru <52287645+CHARANSAIAMURU@users.noreply.github.com>
2026-04-21 19:18:01 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ app.get("/api/health", (req, res) => {
 });
 
 app.use("/api/trucks", truckRoutes);
-
+console.log("MONGO_URI =", process.env.MONGO_URI);
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
